9a7c916931e6078eafe85e0f27e3ab0aab170e6e
Tony Ehlert <aehlert@dmacc.edu>
2023-02-09 09:31:17 -0600

    Finished assignment, initial commit

---
+++ input_while_exit.py
@@ -0,0 +1,62 @@
+"""
+Program: input_while_exit.py
+Author: Tony Ehlert
+Last date modified: 2/9/2023
+
+The purpose of this program is to prompt a user for a number between 1 and 100 and store the input in a list
+and use while loops to check for input validation as well as a sentinel value, then once the sentinel value is entered,
+a for loop is used to print out the contents of the list.  Code with Break commands are contained within each while
+loop to exit the loop once the sentinel value has been entered, instead of running the rest of the while loops
+
+The input is a series of integer numbers from a user
+The output is a print out of the all the numbers the user entered
+"""
+# creation of list variable
+user_input_list = []
+
+# creation of sentinel variable
+SENTINEL_VALUE = "stop"
+
+# creation of variable for user input
+user_input = 0
+
+# creation of float variable for user input, defaulted to 0.0 (a value not within a good range)
+user_input_as_float = 0.0
+
+# while loop and try/except block used to check for valid number entry
+while (user_input != SENTINEL_VALUE):
+
+    # prompt the user for input (indicating the sentinel value to stop)
+    user_input = input(f"Please enter a number between 1 and 100. Enter \"{SENTINEL_VALUE}\" to stop.").lower()
+
+    # if statement to check if sentinel value has been entered and break if it has
+    if (user_input == SENTINEL_VALUE):
+        break
+
+    try:
+        # convert user input to float data type
+        user_input_as_float = float(user_input)
+
+        # while loop checks for valid range
+        while ((user_input_as_float < 1) or (user_input_as_float > 100) and user_input != SENTINEL_VALUE):
+            print("Entry out of range! Number must be between 1 and 100")
+            user_input = input(f"Please enter a number between 1 and 100. Enter \"{SENTINEL_VALUE}\" to stop.").lower()
+
+            # if statement to check if sentinel value has been entered and break if it has
+            if (user_input == SENTINEL_VALUE):
+                break
+            user_input_as_float = float(user_input)
+
+        # if statement to check if sentinel value has been entered and break if it has
+        if (user_input == SENTINEL_VALUE):
+            break
+
+        # add user input to list
+        user_input_list.append(user_input_as_float)
+
+    except:
+        print(f"Cannot convert a string to float: \"{user_input}\"")
+
+# print of the contents of list using for loop
+for number in user_input_list:
+    print(number)
